3975e7f2e9f9eb53ab0da2db76c5fd83f5c60bc9
Kalega1 <kalegin.m@yandex.ru>
2023-05-23 11:40:34 +0500

    наконец-то java закончилась

---
+++ Каталог.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Каталог extends Ноут {
+
+    public Каталог(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+ 
+}
